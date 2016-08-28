f15f9f171a3536d4a840c92db7d78a0258fee3ac
Harry John <harry671003@gmail.com>
2016-08-28 22:57:47 +0530

    75% theme complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,42 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	rename = require('gulp-rename'),
 	concat = require('gulp-concat'),
-	runSequence = require('run-sequence');
+	runSequence = require('run-sequence').use(gulp),
+	plumber = require('gulp-plumber'),
+	gutil = require('gulp-util');
 
 var themeName = "minimalui";
-
 var outputDir = './dist/' + themeName;
-
-var deploymentDir = '../codecup.io/content/themes/'
-
+var deploymentDir = '../codecup.io/content/themes/';
 
 gulp.task('less', function() {
 	return gulp.src('./styles/main.less')
-		.pipe(less())
+		.pipe(plumber())
+		.pipe(less().on('error', function(err){
+			gutil.log(err);
+			this.emit('end');
+		}))
 		.pipe(rename('style.css'))
 		.pipe(gulp.dest(outputDir + '/assets/styles/'));
 });
 
 gulp.task('js', function() {
 	return gulp.src('./scripts/**/*.js')
+		.pipe(plumber())
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest(outputDir + '/assets/scripts/'));
 });
 
 gulp.task('assets', function() {
-	return gulp.src('./assets/**/*').pipe(gulp.dest(outputDir + '/assets'))
+	return gulp.src('./assets/**/*')
+		.pipe(plumber())
+		.pipe(gulp.dest(outputDir + '/assets'))
 });
 
 gulp.task('templates', function() {
-	return gulp.src('./templates/**/*').pipe(gulp.dest(outputDir))
+	return gulp.src('./templates/**/*')
+		.pipe(plumber())
+		.pipe(gulp.dest(outputDir))
 })
 
 
@@ -39,12 +47,14 @@ gulp.task('build', ['less', 'js', 'assets', 'templates']);
 
 // Deploy the files to deploymentDir
 gulp.task('deploy', function() {
-	return gulp.src(outputDir + '/**/*').pipe(gulp.dest(deploymentDir + themeName));
+	return gulp.src(outputDir + '/**/*')
+		.pipe(plumber())
+		.pipe(gulp.dest(deploymentDir + themeName));
 });
 
 // Watch server
 gulp.task('watch', function() {
-	gulp.watch(['./styles/**/*.less', './scripts/**/*.js'], function() {
+	gulp.watch(['./styles/**/*', './scripts/**/*', './templates/**/*', './assets/**/*'], function() {
 		runSequence('build', 'deploy');
 	});
 });
